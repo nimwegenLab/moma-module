1443ef16a95852df6fe7ba8427c580b8e016f4d3
Michael Mell <michael.mell@unibas.ch>
2023-09-04 16:53:10 +0200

    Add module version 0.9.8.

---
+++ 0.9.8.lua
@@ -0,0 +1,22 @@
+help([==[
+
+Description
+===========
+Mother-machine analyzer (MoMA) tool to track bacteria in Mother machines.
+
+More information
+================
+ - Homepage: https://github.com/michaelmell/MoMA
+ - Wiki: https://github.com/michaelmell/MoMA/wiki
+]==])
+
+whatis([==[Description: Mother-machine analyzer (MoMA)]==])
+whatis([==[Homepage: https://github.com/michaelmell/MoMA]==])
+
+conflict("moma-module")
+
+-- This gets the path to module files relative the Lua definition file as described here: https://lmod.readthedocs.io/en/6.6/100_generic_modules.html
+local fn = myFileName()
+
+pkgPath = string.gsub(fn,".lua","")
+prepend_path("PATH", pkgPath)
